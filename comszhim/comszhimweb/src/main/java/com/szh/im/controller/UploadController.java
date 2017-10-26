baa2d51a777bef93815862e57300d014ac30f6da
sunzhuhuan <sunzhuhuan@jiangduoduo.com>
2017-10-26 22:48:05 +0800

    csv

--- comszhim/comszhimweb/src/main/java/com/szh/im/controller/UploadController.java
+++ comszhim/comszhimweb/src/main/java/com/szh/im/controller/UploadController.java
@@ -1,6 +1,7 @@
 package com.szh.im.controller;
 
 
+import org.jboss.netty.handler.codec.http.HttpResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,9 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -26,27 +31,27 @@ public class UploadController {
 
     public static ExecutorService pool = Executors.newFixedThreadPool(20);
 
-    @RequestMapping(value = "/download",method = RequestMethod.GET)
-    public void uploadFile(@RequestParam(value = "url") String url,@RequestParam(value = "dir") String dir) {
+    @RequestMapping(value = "/download", method = RequestMethod.GET)
+    public void uploadFile(@RequestParam(value = "url") String url, @RequestParam(value = "dir") String dir) {
         String book = "/Users/alongsea2/Documents/books/hentai/" + dir;
         File f = new File(book);
-        if(!f.exists()){
+        if (!f.exists()) {
             f.mkdir();
             f.setWritable(true);
         }
         Pattern pattern = Pattern.compile(".*\\/(.*).thumb.jpg.*");
         Matcher matcher = pattern.matcher(url);
         String s = null;
-        if(matcher.find()){
+        if (matcher.find()) {
             s = matcher.group(1);
         }
         File pic = new File(book + "/" + s + ".jpg");
-        if(!pic.exists()){
-            pool.execute(()->{
+        if (!pic.exists()) {
+            pool.execute(() -> {
                 try {
                     InputStream inputStream = null;
                     URL realUrl = new URL(url);
-                    HttpURLConnection connection = (HttpURLConnection)realUrl.openConnection();
+                    HttpURLConnection connection = (HttpURLConnection) realUrl.openConnection();
                     // 设置通用的请求属性
                     connection.setRequestProperty("accept", "*/*");
                     connection.setRequestProperty("connection", "Keep-Alive");
@@ -59,7 +64,7 @@ public class UploadController {
                         BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(pic));
                         BufferedInputStream bufferInputStream = new BufferedInputStream(inputStream);
                         int n;
-                        while ( (n = bufferInputStream.read()) != -1){
+                        while ((n = bufferInputStream.read()) != -1) {
                             bufferedOutputStream.write(n);
                         }
                         bufferedOutputStream.flush();
@@ -83,13 +88,42 @@ public class UploadController {
 
 
     @RequestMapping(value = "/index")
-    public ModelAndView index(){
+    public ModelAndView index() {
         return new ModelAndView("look");
     }
 
-    public static void main(String[] args) {
-        Pattern pattern = Pattern.compile(".*\\/(.*).thumb.jpg.*");
-        Matcher matcher = pattern.matcher("http://t.fakku.net/images/manga/t/the-flat-c-major-english_1499426443/thumbs/001.thumb.jpg?f=1499871600");
+
+    @RequestMapping(value = "/csv", method = RequestMethod.GET)
+    public void csv(HttpServletResponse response) {
+
+        List<String> list = new ArrayList<>();
+
+        for(int i = 0;i<50000 ;i++){
+            list.add(i + "");
+        }
+
+        response.setContentType("text/csv;charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Content-Disposition",
+                "attachment;filename=test.csv");
+        try {
+            OutputStream outputStream = response.getOutputStream();
+            final byte[] bom = new byte[] { (byte)0xEF, (byte)0xBB, (byte)0xBF };
+            for (byte b : bom) {
+                outputStream.write(b);
+            }
+
+            for (String s : list) {
+                outputStream.write((s + ",\n").getBytes());
+            }
+
+            outputStream.flush();
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
+
 }
