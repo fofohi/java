49ce7d234080b8c1d1fd7da944487771c6443e61
23809758@qq.com <23809758@qq.com>
2018-12-17 13:24:38 +0800

    for 4

--- springdemo/src/test/main/java/TestCaseFor4.java
+++ springdemo/src/test/main/java/TestCaseFor4.java
@@ -1,31 +1,33 @@
-/*
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
-@SpringJUnitConfig(locations = {"classpath:service.xml","classpath:mvc.xml"})
-@WebAppConfiguration()
-@ExtendWith(SpringExtension.class)
-class TestCase {
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = {"classpath:service.xml","classpath:mvc.xml"})
+public class TestCaseFor4 {
 
     private MockMvc mockMvc;
 
     @Autowired
     private WebApplicationContext wac;
 
-    @BeforeEach
-    void setup() {
+    @Before
+    public void setup() {
+        System.out.println(wac);
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
     }
+
     @Test
     public void test(){
         try {
@@ -36,5 +38,5 @@ class TestCase {
         }
     }
 
+
 }
-*/
